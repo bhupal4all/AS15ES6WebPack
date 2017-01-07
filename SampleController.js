78cc6894a7d9c44ea0b164c9686de15b011bf2e9
bhupal <bhupal4all@gmail.com>
2017-01-07 15:57:06 +0530

    adding SampleController.js

---
+++ SampleController.js
@@ -0,0 +1,8 @@
+class SampleController {
+	constructor() {
+		this.numbers=[1,2,3,4,5,6,7,8,9,10];
+		this.title = 'Loading from SampleController';
+	}
+}
+
+export default SampleController
--- app.module.js
+++ app.module.js
@@ -1,6 +1,11 @@
 import angular from 'angular';
 import usersList from './components/usersList.component.js';
+import SampleController from './SampleController.js'
 
 angular
     .module('myApp', [])
-    .component('usersList', usersList);
+    .component('usersList', usersList)
+	.controller('SampleController',SampleController)
+	.run(function(){
+		console.log('ready');
+	});
